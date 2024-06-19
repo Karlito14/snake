e385e5c60b59348f2a1004f11293fb93506d2a25
Karlito14 <leiroz26@hotmail.com>
2024-06-19 09:57:23 +0200

    dessin du map et du snake

--- src/main.js
+++ src/main.js
@@ -2,3 +2,24 @@ import './style.css';
 
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
+const grid = 25;
+const snake = [
+  [9, 9],
+  [8, 9],
+  [7, 9],
+];
+
+const drawMap = () => {
+  context.fillStyle = 'black';
+  context.fillRect(0, 0, 800, 500);
+};
+
+const drawSnake = () => {
+    context.fillStyle = 'green';
+    for(let body of snake) {
+        context.fillRect(body[0] * grid, body[1] * grid, grid, grid)
+    }
+}
+
+drawMap();
+drawSnake()
